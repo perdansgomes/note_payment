165dcde0e4d43b98d411d9cfc384185b80c284cc
Pedro Daniel <contacomercialpedrodaniel@gmail.com>
2023-09-08 18:43:47 -0300

    first commit

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+import mysql.connector
+import random
+
+app = Flask(__name__)
+
+#Routes HTML
+@app.route('/')
+@app.route('/index')
+def index():
+    random_number = random.randint(1, 1000)
+    return render_template('index.html', random_number = random_number)
+
+#Routes End
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
